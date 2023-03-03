94da7d4765b6ea9a3fd853670a739b7689467ef5
Mike Babb <mike7400@gmail.com>
2023-03-03 04:54:29 -0500

    erm

--- vite.config.ts
+++ vite.config.ts
@@ -48,7 +48,7 @@ export default defineConfig(({ mode }) => ({
     },
 
     plugins: [
-        dts({ insertTypesEntry: true }),
+        dts(),
         vitePluginIfDef.default({
             define: {
                 DEBUG: true,
